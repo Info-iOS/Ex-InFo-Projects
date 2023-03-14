3f238c8d5401ac14f12b7a565ba90704e3dabecf
junha <goodjunha@gmail.com>
2023-03-14 20:04:13 +0900

    Then과 snpkit을 사용한 CollectionView 입니다.

---
+++ CollectionView/ExCollectionView+code/ExCollectionView+code/customCell.swift
@@ -0,0 +1,42 @@
+//
+//  customCell.swift
+//  ExCollectionView+code
+//
+//  Created by 박준하 on 2023/03/14.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class customCell: UICollectionViewCell {
+
+    var backView = UIImageView().then {
+        $0.backgroundColor = .blue
+    }
+    var label = UILabel().then {
+        $0.textColor = .black
+        $0.textAlignment = .center
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(backView)
+        contentView.addSubview(label)
+        
+        backView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        label.snp.makeConstraints {
+            $0.centerY.centerX.equalToSuperview()
+        }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
