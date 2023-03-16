18be1825741bc85157ba5b561ae4e64797cf1d50
junha <goodjunha@gmail.com>
2023-03-16 23:24:10 +0900

    화면 전환 storyboard

---
+++ ScreenTransition/ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by 박준하 on 2023/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
