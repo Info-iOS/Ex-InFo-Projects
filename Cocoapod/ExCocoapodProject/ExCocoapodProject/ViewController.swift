e46d0f913e918380dead2c55d05cd9514218064d
eunho0922 <tylereunholee010203@gmail.com>
2023-03-26 22:29:11 +0900

    코코아팟을 사용한 라이브러리 다운로드 방법

---
+++ Cocoapod/ExCocoapodProject/ExCocoapodProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ExCocoapodProject
+//
+//  Created by 이은호 on 2023/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
