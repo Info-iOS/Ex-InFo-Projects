fe794138e4f4d8864770077792fcadc03bba75d0
junha <goodjunha@gmail.com>
2023-05-30 14:19:25 +0900

    설명주석 추가

--- Network/exampleAlamofire/exampleAlamofire/ViewController.swift
+++ Network/exampleAlamofire/exampleAlamofire/ViewController.swift
@@ -20,17 +20,21 @@ class ViewController: UIViewController {
             "Authorization": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdW5oYSIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE2ODU0MjIxMjksImV4cCI6MTY4NTQyMzkyOX0.l9B6DRPpyyzd1DcmIVQBqEAutypSYSoXG2lf6DAfUZM"
         ]
 
+        // url 지정 및 메소드 랑 받는 곳
         AF.request("http://www.mukgen.info/meal/today", method: .get, headers: headers)
             .responseDecodable(of: MealResponse.self) { response in
                 switch response.result {
                 case let .success(mealResponse):
+                    // 성공했을 경우
                     for item in mealResponse.responseList {
+                        //급식 리스트 출력
                         print("Rice Type: \(item.riceType)")
                         print("Item: \(item.item)")
                         print("Rice ID: \(item.riceId)")
                         print("--------------------")
                     }
                 case let .failure(error):
+                    // 실패했을 경우
                     print("Network request failed: \(error)")
                 }
             }
